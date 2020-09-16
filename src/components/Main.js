5496f74cbd50f5388509a34fef072bb8a4855e17
arskdj <68353557+arskdj@users.noreply.github.com>
2020-09-17 00:30:08 +0300

    10.8

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import { View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 import RepositoryList from './RepositoryList';
 import SignIn from './SignIn';
-import AppBar from './AppBar.js';
+import AppBar from './AppBar';
 import { Route, Switch, Redirect } from 'react-router-native';
 
 const styles = StyleSheet.create({
@@ -20,7 +20,7 @@ const Main = () => {
             <AppBar/>
             <Switch>
                 <Route path="/signin" exact>
-                    <SignIn/>
+                    <SignIn />
                 </Route>
                 <Route path="/" exact>
                     <View style={styles.container}>
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,62 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, TouchableWithoutFeedback, Text } from 'react-native';
+import FormikTextInput from './FormikTextInput'
+import { Formik } from 'formik';
+import { colors } from '../theme'
+
 
 const SignIn = () => {
-    return <Text>The sign in view</Text>;
+    const initialValues = {
+        username: '',
+        password: '',
+    };
+
+    const onSubmit = values => {
+        console.log(values);
+
+    };
+
+    const styles = {
+        form: {
+            flexDirection: 'column',
+            backgroundColor: colors.gray,
+        },
+        text: {
+            marginTop: 10,
+            fontSize: 24,
+            paddingBottom: 20,
+        },
+        button: {
+            marginTop: 10,
+            alignItems: "center",
+            backgroundColor: colors.darkgreen,
+        },
+        buttonText: {
+            fontSize: 24,
+            padding: 20,
+            color: colors.white,
+        },
+    }
+
+    return (
+        <Formik initialValues={initialValues} onSubmit={onSubmit}>
+            {( {handleSubmit} ) => (
+                <View style={styles.form}>
+                    <FormikTextInput 
+                        style = {styles.text}
+                        name="username" placeholder="username" />
+                    <FormikTextInput 
+                        style = {styles.text}
+                        name="password" placeholder="password" />
+                    <TouchableWithoutFeedback onPress={handleSubmit}>
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>Sign In</Text>
+                        </View>
+                    </TouchableWithoutFeedback>
+                </View>
+            )}
+        </Formik>
+    )
 };
 
 export default SignIn;
